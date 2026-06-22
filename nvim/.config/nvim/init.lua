fb9a7369179d74098bd9c2d388683666ff983131
Pannawich Thamart <MadManJJ4949@gmail.com>
2026-06-22 18:27:49 +0700

    feat: remove delete + copy (nvim default)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,10 +2,11 @@ require("config.options")
 require("config.keybinds")
 require("config.lazy")
 vim.opt.clipboard = "unnamedplus"
-vim.cmd([[colorscheme oldschool]])
+vim.cmd([[colorscheme catppuccin]])
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff", bg = "NONE" })
 vim.opt.fileformats = { "unix", "dos" }
 vim.opt.termguicolors = true
 vim.opt_global.fileformat = "unix"
@@ -14,6 +15,13 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 
 vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)", opts)
 vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)", opts)
+
+-- Delete without copying into default register
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete line without yanking" })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without yanking" })
+vim.keymap.set({ "n", "v" }, "C", '"_C', { desc = "Change line without yanking" })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete single char without yanking" })
 
 vim.keymap.set("i", "<C-o>", "<Plug>(emmet-expand-abbr)", { silent = true })
 
