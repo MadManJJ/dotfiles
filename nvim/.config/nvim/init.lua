cbeb0eeb892c5f8ba9740cfc92cc3e9407dfb130
Pannawich Thamart <MadManJJ4949@gmail.com>
2026-07-06 16:12:25 +0700

    feat: update yanking in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,10 +17,10 @@ vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)", opts)
 vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)", opts)
 
 -- Delete without copying into default register
-vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
-vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete line without yanking" })
-vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without yanking" })
-vim.keymap.set({ "n", "v" }, "C", '"_C', { desc = "Change line without yanking" })
+-- vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+-- vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete line without yanking" })
+-- vim.keymap.set({ "n", "v" }, "c", '"_c', { desc = "Change without yanking" })
+-- vim.keymap.set({ "n", "v" }, "C", '"_C', { desc = "Change line without yanking" })
 vim.keymap.set({ "n", "v" }, "x", '"_x', { desc = "Delete single char without yanking" })
 
 vim.keymap.set("i", "<C-o>", "<Plug>(emmet-expand-abbr)", { silent = true })
